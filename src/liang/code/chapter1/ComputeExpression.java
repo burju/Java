2b63ac91de315a225081da19f5f35b6c1fdc3f10
Burju <kanliska@gmail.com>
2023-12-16 00:34:05 -0600

    first Commit?

---
+++ src/liang/code/chapter1/ComputeExpression.java
@@ -0,0 +1,7 @@
+package liang.code.chapter1;
+
+public class ComputeExpression { 
+  public static void main(String[] args) { 
+    System.out.println((10.5 + 2 * 3) / (45 - 3.5));
+  }
+}
---
+++ src/liang/code/chapter1/ShowSyntaxErrors.java
@@ -0,0 +1,7 @@
+package liang.code.chapter1;
+
+public class ShowSyntaxErrors {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+    }
+}
